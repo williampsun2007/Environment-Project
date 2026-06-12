19ab259f4924233643a5845e553bf4dc79ba6b7d
williampsun <williampsun07@gmail.com>
2026-06-11 23:03:10 -0400

    Added programs to design concentration maps for 40 scenarios, and to model change in emissions

---
+++ data/Concentration_Graph_Builder.py
@@ -0,0 +1,179 @@
+import xarray as xr
+from pathlib import Path
+import matplotlib.pyplot as plt
+from cnmaps import get_adm_maps
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import Point
+import shapely
+from matplotlib.patches import Patch
+
+province_to_region = {
+    '北京市': 'BTHS',
+    '天津市': 'BTHS',
+    '河北省': 'BTHS',
+    '山东省': 'BTHS',
+    '河南省': 'BTHS',
+    '山西省': 'FWP',
+    '陕西省': 'FWP',
+    '甘肃省': 'FWP',
+    '宁夏回族自治区': 'FWP',
+    '上海市': 'YRD',
+    '江苏省': 'YRD',
+    '浙江省': 'YRD',
+    '安徽省': 'YRD',
+    '四川省': 'SCB',
+    '重庆市': 'SCB',
+    '广东省': 'PRD',
+}
+
+province_dict = {
+    "北京市": "Beijing",
+    "天津市": "Tianjin",
+    "河北省": "Hebei",
+    "山西省": "Shanxi",
+    "内蒙古自治区": "NeiMonggol",
+    "辽宁省": "Liaoning",
+    "吉林省": "Jilin",
+    "黑龙江省": "Heilongjiang",
+    "上海市": "Shanghai",
+    "江苏省": "Jiangsu",
+    "浙江省": "Zhejiang",
+    "安徽省": "Anhui",
+    "福建省": "Fujian",
+    "江西省": "Jiangxi",
+    "山东省": "Shandong",
+    "河南省": "Henan",
+    "湖北省": "Hubei",
+    "湖南省": "Hunan",
+    "广东省": "Guangdong",
+    "广西壮族自治区": "Guangxi",
+    "海南省": "Hainan",
+    "重庆市": "Chongqing",
+    "四川省": "Sichuan",
+    "贵州省": "Guizhou",
+    "云南省": "Yunan",
+    "西藏自治区": "Xizang",
+    "陕西省": "Shaanxi",
+    "甘肃省": "Gansu",
+    "青海省": "Qinghai",
+    "宁夏回族自治区": "Ningxia",
+    "新疆维吾尔自治区": "Xinjiang"
+}
+
+path_nc_files = Path("NC Files and Emission Reports")
+nc_files = sorted(path_nc_files.glob("*.nc"))
+
+records = []
+for province in get_adm_maps(level='省'):
+    records.append({
+        'province': province['province'],
+        'geometry': province['geometry']
+    })
+
+province_map = gpd.GeoDataFrame(records, crs='EPSG:4326')
+
+ds = xr.open_dataset(nc_files[0])
+lon_flat = ds["lon"].values.flatten()
+lat_flat = ds["lat"].values.flatten()
+
+world = gpd.read_file("https://naturalearth.s3.amazonaws.com/110m_cultural/ne_110m_admin_0_countries.zip")
+china = world[world['NAME'] == 'China']
+in_china_flat = shapely.contains_xy(china.geometry.iloc[0], lon_flat, lat_flat)
+
+points_gdf = gpd.GeoDataFrame(
+    geometry=[Point(lon, lat) for lon, lat in zip(lon_flat, lat_flat)],
+    crs='EPSG:4326'
+)
+
+points_gdf = points_gdf[in_china_flat]
+lon_flat = lon_flat[in_china_flat]
+lat_flat = lat_flat[in_china_flat]
+
+joined = gpd.sjoin(points_gdf, province_map, how='left', predicate='within')
+joined = joined[['province']]
+joined['region'] = joined['province'].map(province_to_region).fillna('Other')
+joined['lon'] = lon_flat
+joined['lat'] = lat_flat
+
+amount_grid_province = joined.groupby('province')['lat'].count()
+
+df_population = pd.read_csv("Population_Projection/Population_Projection_Data/Province/POP_TOTAL.csv")
+df_population = df_population.dropna()
+df_population = df_population[df_population["V2"] == "_SSPFer2_SSPMigr3"]
+
+drop_cols = []
+for col in df_population.columns:
+    if (col != "V1" and col != "2035"):
+        drop_cols.append(col)
+        
+df_population.drop(drop_cols, axis = 1, inplace = True)
+df_population = df_population.set_index("V1")
+
+joined["Province Population"] = joined["province"].apply(lambda x: df_population.loc[province_dict[x]]["2035"] if pd.notna(x) else 0)
+joined["Population Per Cell"] = joined[["province", "Province Population"]].apply(
+    lambda x: x[1] / amount_grid_province.loc[x[0]] if pd.notna(x[0]) else 0, axis=1
+)
+
+region_colors = {
+    'BTHS': 'red',
+    'FWP': 'orange',
+    'YRD': 'green',
+    'SCB': 'purple',
+    'PRD': 'blue',
+    'Other': '#D3D3D3'
+}
+
+for file in nc_files:
+    ds = xr.open_dataset(file)
+    
+    pm25_data = ds["pred_PM25"].mean(dim="time").values.flatten()[in_china_flat]
+    joined["pm25"] = pm25_data
+
+    plot_df = joined.dropna(subset=['province']).copy()
+
+    plot_df = plot_df.sort_values('pm25').reset_index(drop=True)
+
+    plot_df['cum_pop'] = plot_df['Population Per Cell'].cumsum()
+    plot_df['cum_pop_frac'] = plot_df['cum_pop'] / plot_df['Population Per Cell'].sum() * 100
+
+    total_pop = plot_df['Population Per Cell'].sum()
+    plot_df['bar_width'] = plot_df['Population Per Cell'] / total_pop * 100
+    
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.fill_between(plot_df['cum_pop_frac'], plot_df['pm25'], color='#D3D3D3', step='post')
+
+    region_df = plot_df[plot_df['region'] != 'Other']
+    ax.bar(region_df['cum_pop_frac'], region_df['pm25'], color=region_df['region'].map(region_colors), 
+           width=region_df['bar_width'], align='edge', linewidth=0)
+
+    ax.plot(plot_df['cum_pop_frac'], plot_df['pm25'], color='black', linewidth=0.5)
+
+    ax.axhline(25, linestyle='--', color='gray', label='25 μg/m³')
+
+    ax.set_xlabel('Population Fraction (%)')
+    ax.set_ylabel('PM2.5 Exposure (μg/m³)')
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0)
+
+    legend_elements = [
+        Patch(facecolor='red', label='Beijing-Tianjin-Hebei and Surroundings'),
+        Patch(facecolor='orange', label='Fenwei Plain'),
+        Patch(facecolor='green', label='Yangtze River Delta'),
+        Patch(facecolor='purple', label='Sichuan Basin'),
+        Patch(facecolor='blue', label='Pearl River Delta'),
+        Patch(facecolor='#D3D3D3', label='Other'),
+    ]
+
+    ax.legend(handles=legend_elements, loc='upper left')
+
+    plt.title(f"PM2.5 Population Exposure for Scenario {file.stem}")
+    plt.tight_layout()
+    plt.savefig(f"Emission Files/40 Scenario Concentration Maps/{file.stem}_exposure_curve.png", dpi=300)
+    
+print("Finished!")
+    
+    
+    
+    
+    
---
+++ data/Scenario_Province_Pollutant_Difference.py
@@ -0,0 +1,139 @@
+import cartopy.crs as ccrs
+import openpyxl
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+from cnmaps import get_adm_maps
+import geopandas as gpd
+
+wb_baseline = openpyxl.load_workbook("baseline/2020_2035_Raw.xlsx")
+wb_clean_air = openpyxl.load_workbook("clean_air/2020_2035_Raw.xlsx")
+wb_otpca = openpyxl.load_workbook("on-time_peak-clean_air/2020_2035_Raw.xlsx")
+wb_otpnzca = openpyxl.load_workbook("on-time_peak-net_zero-clean_air/2020_2035_Raw.xlsx")
+wb_epnzca = openpyxl.load_workbook("early_peak-net_zero-clean_air/2020_2035_Raw.xlsx")
+
+workbook_arr = [wb_baseline, wb_clean_air, wb_otpca, wb_otpnzca, wb_epnzca]
+
+province_arr = ["Beijing", "Tianjin", "Hebei", "Shanxi", "Inner Mongolia", "Liaoning", "Jilin", "Heilongjiang", "Shanghai", 
+                "Jiangsu", "Zhejiang", "Anhui", "Fujian", "Jiangxi", "Shandong", "Henan", "Hubei", "Hunan", "Guangdong", 
+                "Guangxi", "Hainan", "Chongqing", "Sichuan", "Guizhou", "Yunnan", "Tibet", "Shaanxi", "Gansu", "Qinghai",
+                "Ningxia", "Xinjiang"]
+
+province_map = {
+    "北京市": "Beijing",
+    "天津市": "Tianjin",
+    "河北省": "Hebei",
+    "山西省": "Shanxi",
+    "内蒙古自治区": "Inner Mongolia",
+    "辽宁省": "Liaoning",
+    "吉林省": "Jilin",
+    "黑龙江省": "Heilongjiang",
+    "上海市": "Shanghai",
+    "江苏省": "Jiangsu",
+    "浙江省": "Zhejiang",
+    "安徽省": "Anhui",
+    "福建省": "Fujian",
+    "江西省": "Jiangxi",
+    "山东省": "Shandong",
+    "河南省": "Henan",
+    "湖北省": "Hubei",
+    "湖南省": "Hunan",
+    "广东省": "Guangdong",
+    "广西壮族自治区": "Guangxi",
+    "海南省": "Hainan",
+    "重庆市": "Chongqing",
+    "四川省": "Sichuan",
+    "贵州省": "Guizhou",
+    "云南省": "Yunnan",
+    "西藏自治区": "Tibet",
+    "陕西省": "Shaanxi",
+    "甘肃省": "Gansu",
+    "青海省": "Qinghai",
+    "宁夏回族自治区": "Ningxia",
+    "新疆维吾尔自治区": "Xinjiang"
+}
+
+pollutant_norms = {
+    "SO2":   mcolors.SymLogNorm(linthresh=10000, vmin=-520000, vmax=200000),
+    "NOX":   mcolors.SymLogNorm(linthresh=10000, vmin=-400000, vmax=180000),
+    "VOC":   mcolors.SymLogNorm(linthresh=10000, vmin=-660000, vmax=260000),
+    "NH3":   mcolors.SymLogNorm(linthresh=1000,  vmin=-180000, vmax=30000),
+    "PM2.5": mcolors.SymLogNorm(linthresh=5000,  vmin=-310000, vmax=110000),
+    "PM10":  mcolors.SymLogNorm(linthresh=5000,  vmin=-380000, vmax=140000),
+    "BC":    mcolors.SymLogNorm(linthresh=1000,  vmin=-80000,  vmax=30000),
+    "OC":    mcolors.SymLogNorm(linthresh=1000,  vmin=-160000, vmax=60000),
+}
+
+pollutant_ticks = {
+    "SO2": [-520000, -100000, -10000, 0, 10000, 50000, 100000, 200000],
+    "NOX": [-400000, -100000, -10000, 0, 10000, 50000, 100000, 180000],
+    "VOC": [-660000, -100000, -10000, 0, 10000, 50000, 100000, 260000],
+    "NH3":   [-180000, -10000, -1000, 0, 1000, 10000, 30000],
+    "PM2.5": [-310000, -50000, -5000, 0, 5000, 50000, 110000],
+    "PM10": [-380000, -50000, -5000, 0, 5000, 20000, 50000, 140000],
+    "BC":    [-80000, -10000, -1000, 0, 1000, 10000, 30000],
+    "OC": [-160000, -10000, -1000, 0, 1000, 10000, 30000, 60000]
+}
+
+pollutant_ticklabels = {
+    "SO2": ['-520K', '-100K', '-10K', '0', '+10K', '+50K', '+100K', '+200K'],
+    "NOX": ['-400K', '-100K', '-10K', '0', '+10K', '+50K', '+100K', '+180K'],
+    "VOC": ['-660K', '-100K', '-10K', '0', '+10K', '+50K', '+100K', '+260K'],
+    "NH3":   ['-180K', '-10K', '-1K', '0', '+1K', '+10K', '+30K'],
+    "PM2.5": ['-310K', '-50K', '-5K', '0', '+5K', '+50K', '+110K'],
+    "PM10": ['-380K', '-50K', '-5K', '0', '+5K', '+20K', '+50K', '+140K'],
+    "BC":    ['-80K', '-10K', '-1K', '0', '+1K', '+10K', '+30K'],
+    "OC": ['-160K', '-10K', '-1K', '0', '+1K', '+10K', '+30K', '+60K']
+}
+
+records = []
+for province in get_adm_maps(level='省'):
+    records.append({
+        'province': province['province'],
+        'geometry': province['geometry']
+    })
+
+gdf = gpd.GeoDataFrame(records, crs='EPSG:4326')
+
+for pollutant in ["SO2", "NOX", "VOC", "NH3", "PM2.5", "PM10", "BC", "OC"]:
+    cmap = cm.RdYlGn
+    norm = pollutant_norms[pollutant]
+    
+    fig, ax = plt.subplots(nrows = 5, ncols = 5, figsize = (30, 25), subplot_kw={'projection': ccrs.PlateCarree()})
+    
+    for index, sector in enumerate(["Power", "Industry", "Transportation", "Residential", "Agriculture"]):
+        for index_2, workbook in enumerate(workbook_arr):
+            sheet = workbook[pollutant]
+            
+            def get_color(name):
+                if name in province_map and name not in ["台湾省", "香港特别行政区", "澳门特别行政区"]:
+                    english_name = province_map[name]
+                    province_index = province_arr.index(english_name)
+                    difference = sheet.cell(index + 12, province_index + 3).value - sheet.cell(index + 3, province_index + 3).value
+                    return cmap(norm(difference))
+                       
+                return 'lightgrey'   
+            
+            gdf['color'] = gdf['province'].apply(get_color)
+            gdf.plot(color = gdf['color'], ax = ax[index][index_2], edgecolor='black', linewidth=0.1)
+            
+    plt.subplots_adjust(hspace=0.1, wspace=0.05)
+    
+    sm = cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    cbar = plt.colorbar(sm, ax=ax, orientation='vertical', pad=0.02)
+    cbar.set_label('Emission Change 2020-2035 (tons)', fontsize=14)
+    cbar.set_ticks(pollutant_ticks[pollutant])
+    cbar.set_ticklabels(pollutant_ticklabels[pollutant])
+    
+    for index, scenario in enumerate(["Baseline", "CleanAir", "OTPCA", "OTPNZCA", "EPNZCA"]):
+        ax[0][index].set_title(scenario)
+    for index, sector in enumerate(["Power", "Industry", "Transportation", "Residential", "Agriculture"]):
+        ax[index][0].text(-0.15, 0.5, sector, transform=ax[index][0].transAxes,
+                      fontsize=14, va='center', rotation='vertical')
+
+    fig.suptitle(f'Emission Change from 2020 to 2035 for {pollutant}', fontsize=18, fontweight='bold')
+    plt.savefig(f"Emission Files/Pollutant Change Province Maps/{pollutant}_Map.png")
+    plt.close(fig)
+    
+print("Finished!")
